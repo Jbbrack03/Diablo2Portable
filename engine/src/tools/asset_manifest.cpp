540baa740101ffdcffa95f2d33c6f00cae71105a
Josh Brackin <Jbbrack03@gmail.com>
2025-06-25 09:08:25 -0500

    Add AssetManifest version management support - RED/GREEN cycle complete

--- engine/src/tools/asset_manifest.cpp
+++ engine/src/tools/asset_manifest.cpp
@@ -61,9 +61,20 @@ bool AssetManifest::load(const std::string& path) {
     // Simple JSON parsing for testing
     // In production, use a proper JSON library
     assets.clear();
+    manifestVersion = 1; // Reset to default
     
     std::string line;
     while (std::getline(file, line)) {
+        // Look for version line
+        if (line.find("\"version\":") != std::string::npos) {
+            size_t colonPos = line.find(":");
+            size_t commaPos = line.find(",", colonPos);
+            std::string versionStr = line.substr(colonPos + 1, commaPos - colonPos - 1);
+            // Trim whitespace
+            versionStr.erase(0, versionStr.find_first_not_of(" \t"));
+            versionStr.erase(versionStr.find_last_not_of(" \t") + 1);
+            manifestVersion = std::stoi(versionStr);
+        }
         // Look for path lines
         if (line.find("\"path\":") != std::string::npos) {
             size_t start = line.find("\"", line.find("\"path\":") + 7) + 1;
@@ -73,7 +84,7 @@ bool AssetManifest::load(const std::string& path) {
             AssetInfo info;
             info.path = assetPath;
             
-            // Read next lines for size and checksum
+            // Read next lines for size, checksum, type, version
             std::getline(file, line); // size line
             if (line.find("\"size\":") != std::string::npos) {
                 size_t colonPos = line.find(":");
@@ -87,6 +98,33 @@ bool AssetManifest::load(const std::string& path) {
                 size_t start = line.find("\"", line.find("\"checksum\":") + 10) + 1;
                 size_t end = line.find("\"", start);
                 info.checksum = line.substr(start, end - start);
+            }
+            
+            // Check if there are more fields (type and version)
+            // Need to peek at the next line to see if it's continuing the asset
+            std::streampos oldPos = file.tellg();
+            if (std::getline(file, line)) {
+                if (line.find("\"type\":") != std::string::npos) {
+                    size_t start = line.find("\"", line.find("\"type\":") + 6) + 1;
+                    size_t end = line.find("\"", start);
+                    info.type = line.substr(start, end - start);
+                    
+                    // Check for version on next line
+                    if (std::getline(file, line) && line.find("\"version\":") != std::string::npos) {
+                        size_t colonPos = line.find(":");
+                        std::string afterColon = line.substr(colonPos + 1);
+                        // Find first digit
+                        size_t digitStart = afterColon.find_first_of("0123456789");
+                        if (digitStart != std::string::npos) {
+                            size_t digitEnd = afterColon.find_first_not_of("0123456789", digitStart);
+                            std::string versionStr = afterColon.substr(digitStart, digitEnd - digitStart);
+                            info.version = std::stoi(versionStr);
+                        }
+                    }
+                } else {
+                    // This line is not part of the current asset, seek back
+                    file.seekg(oldPos);
+                }
             }
             
             assets[assetPath] = info;
--- engine/tests/tools/asset_manifest_test.cpp
+++ engine/tests/tools/asset_manifest_test.cpp
@@ -2,6 +2,7 @@
 #include "tools/asset_manifest.h"
 #include <filesystem>
 #include <fstream>
+#include <algorithm>
 
 namespace fs = std::filesystem;
 using namespace d2;
@@ -35,4 +36,125 @@ TEST_F(AssetManifestTest, GenerateManifest) {
     
     EXPECT_EQ(loaded.getAssetCount(), 2);
     EXPECT_TRUE(loaded.hasAsset("sprites/player.png"));
+}
+
+TEST_F(AssetManifestTest, GetAssetInfo) {
+    AssetManifest manifest;
+    
+    // Add some test assets
+    manifest.addAsset("sprites/player.png", 1024, "checksum1");
+    manifest.addAsset("sounds/music.ogg", 2048, "checksum2");
+    manifest.addAsset("data/config.json", 512, "checksum3");
+    
+    // Test getting existing asset info
+    const auto* info = manifest.getAssetInfo("sprites/player.png");
+    ASSERT_NE(info, nullptr);
+    EXPECT_EQ(info->path, "sprites/player.png");
+    EXPECT_EQ(info->size, 1024);
+    EXPECT_EQ(info->checksum, "checksum1");
+    EXPECT_EQ(info->type, "sprite");  // Should be detected from .png extension
+    
+    // Test getting another asset
+    const auto* soundInfo = manifest.getAssetInfo("sounds/music.ogg");
+    ASSERT_NE(soundInfo, nullptr);
+    EXPECT_EQ(soundInfo->path, "sounds/music.ogg");
+    EXPECT_EQ(soundInfo->size, 2048);
+    EXPECT_EQ(soundInfo->checksum, "checksum2");
+    EXPECT_EQ(soundInfo->type, "sound");  // Should be detected from .ogg extension
+    
+    // Test getting non-existent asset
+    const auto* notFound = manifest.getAssetInfo("does/not/exist.txt");
+    EXPECT_EQ(notFound, nullptr);
+}
+
+TEST_F(AssetManifestTest, GetAssetsByType) {
+    AssetManifest manifest;
+    
+    // Add various types of assets
+    manifest.addAsset("sprites/player.png", 1024, "check1");
+    manifest.addAsset("sprites/monster.png", 2048, "check2");
+    manifest.addAsset("sprites/item.webp", 512, "check3");
+    manifest.addAsset("sounds/music.ogg", 4096, "check4");
+    manifest.addAsset("sounds/effect.wav", 1024, "check5");
+    manifest.addAsset("data/config.json", 256, "check6");
+    manifest.addAsset("data/strings.txt", 128, "check7");
+    manifest.addAsset("misc/readme.md", 64, "check8");  // Unknown type
+    
+    // Get all sprites
+    auto sprites = manifest.getAssetsByType("sprite");
+    EXPECT_EQ(sprites.size(), 3);
+    EXPECT_TRUE(std::find(sprites.begin(), sprites.end(), "sprites/player.png") != sprites.end());
+    EXPECT_TRUE(std::find(sprites.begin(), sprites.end(), "sprites/monster.png") != sprites.end());
+    EXPECT_TRUE(std::find(sprites.begin(), sprites.end(), "sprites/item.webp") != sprites.end());
+    
+    // Get all sounds
+    auto sounds = manifest.getAssetsByType("sound");
+    EXPECT_EQ(sounds.size(), 2);
+    EXPECT_TRUE(std::find(sounds.begin(), sounds.end(), "sounds/music.ogg") != sounds.end());
+    EXPECT_TRUE(std::find(sounds.begin(), sounds.end(), "sounds/effect.wav") != sounds.end());
+    
+    // Get all data files
+    auto data = manifest.getAssetsByType("data");
+    EXPECT_EQ(data.size(), 2);
+    EXPECT_TRUE(std::find(data.begin(), data.end(), "data/config.json") != data.end());
+    EXPECT_TRUE(std::find(data.begin(), data.end(), "data/strings.txt") != data.end());
+    
+    // Get unknown type
+    auto unknown = manifest.getAssetsByType("unknown");
+    EXPECT_EQ(unknown.size(), 1);
+    EXPECT_TRUE(std::find(unknown.begin(), unknown.end(), "misc/readme.md") != unknown.end());
+    
+    // Get non-existent type
+    auto empty = manifest.getAssetsByType("nonexistent");
+    EXPECT_EQ(empty.size(), 0);
+}
+
+TEST_F(AssetManifestTest, GetTotalSize) {
+    AssetManifest manifest;
+    
+    // Empty manifest should have 0 size
+    EXPECT_EQ(manifest.getTotalSize(), 0);
+    
+    // Add some assets
+    manifest.addAsset("file1.png", 1024, "check1");
+    EXPECT_EQ(manifest.getTotalSize(), 1024);
+    
+    manifest.addAsset("file2.ogg", 2048, "check2");
+    EXPECT_EQ(manifest.getTotalSize(), 3072);  // 1024 + 2048
+    
+    manifest.addAsset("file3.json", 512, "check3");
+    EXPECT_EQ(manifest.getTotalSize(), 3584);  // 1024 + 2048 + 512
+    
+    // Add a large file
+    manifest.addAsset("bigfile.dat", 1048576, "check4");  // 1MB
+    EXPECT_EQ(manifest.getTotalSize(), 1052160);  // 3584 + 1048576
+    
+    // Clear and verify
+    manifest.clear();
+    EXPECT_EQ(manifest.getTotalSize(), 0);
+}
+
+TEST_F(AssetManifestTest, VersionManagement) {
+    AssetManifest manifest;
+    
+    // Default version should be 1
+    EXPECT_EQ(manifest.getVersion(), 1);
+    
+    // Set a new version
+    manifest.setVersion(5);
+    EXPECT_EQ(manifest.getVersion(), 5);
+    
+    // Add some assets
+    manifest.addAsset("file1.png", 1024, "check1");
+    manifest.addAsset("file2.ogg", 2048, "check2");
+    
+    // Save with version
+    auto manifestPath = tempPath / "versioned_manifest.json";
+    manifest.save(manifestPath.string());
+    
+    // Load and verify version is preserved
+    AssetManifest loaded;
+    loaded.load(manifestPath.string());
+    EXPECT_EQ(loaded.getVersion(), 5);
+    EXPECT_EQ(loaded.getAssetCount(), 2);
 }
