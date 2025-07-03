1232418f82de5818a90d4ac8fa2e967a7a431d9e
Josh Brackin <jbbrack03@gmail.com>
2025-07-03 09:29:27 -0500

    Add OnboardingActivity welcome screen - RED/GREEN cycle complete

---
+++ android/app/src/androidTest/java/com/diablo2portable/OnboardingActivityTest.java
@@ -0,0 +1,37 @@
+package com.diablo2portable;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class OnboardingActivityTest {
+    
+    @Test
+    public void testWelcomeScreen() {
+        // Launch the onboarding activity
+        ActivityScenario<OnboardingActivity> scenario = 
+            ActivityScenario.launch(OnboardingActivity.class);
+        
+        // Check that welcome title is displayed
+        onView(withId(R.id.welcome_title))
+            .check(matches(withText("Welcome to Diablo II Portable")));
+        
+        // Check that continue button is displayed
+        onView(withId(R.id.continue_button))
+            .check(matches(isDisplayed()));
+    }
+}
