95b1ea85a6160f13e6bdf1cf49cb1375babef8f7
Josh Brackin <jbbrack03@gmail.com>
2025-07-10 14:36:29 -0500

    Add ExtractionHelpDialog implementation file and update CMakeLists.txt

---
+++ engine/src/ui/extraction_help_dialog.cpp
@@ -0,0 +1,10 @@
+#include "ui/extraction_help_dialog.h"
+#include "ui/help_system.h"
+
+namespace d2 {
+
+// Implementation file for ExtractionHelpDialog
+// Currently all methods are inline in the header, but this file
+// is created for future expansion and better architecture
+
+} // namespace d2
